755736a684053fb6139bc720bbe5d6f6078f99d9
Benjamin Carlson <carlson.j.ben@gmail.com>
2017-10-13 01:15:13 -0400

    Initial Commit, prints are weird with 6 threads

---
+++ sppdesktop.py
@@ -0,0 +1,42 @@
+from bluetooth import *
+import threading
+
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+advertise_service( server_sock, "SampleServer",
+                   service_id = uuid,
+                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                   profiles = [ SERIAL_PORT_PROFILE ])
+
+def bluetoothWorker(idx):
+    print("Waiting for connection on RFCOMM channel %d" % port)
+
+    client_sock, client_info = server_sock.accept()
+    print("Accepted connection from ", client_info)
+
+    try:
+        while True:
+            data = client_sock.recv(1024)
+            if len(data) == 0: break
+            print("received from {} {}".format(idx, data))
+            client_sock.send(data)
+    except IOError:
+        pass
+
+    client_sock.close()
+    server_sock.close()
+
+
+threads = []
+for i in range(2):
+    t = threading.Thread(target=bluetoothWorker, args=(i,))
+    threads.append(t)
+    t.start()
+
+print("all done")
