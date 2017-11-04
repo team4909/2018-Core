1a882332418db78f52206c2d6b67b03987837031
Roshan Ravi <roshanravi10@gmail.com>
2017-11-04 15:21:48 -0400

    Added Error Handling, Allows Reconnects

--- sppdesktop.py
+++ sppdesktop.py
@@ -1,42 +1,46 @@
 from bluetooth import *
 import threading
 
-server_sock=BluetoothSocket( RFCOMM )
-server_sock.bind(("",PORT_ANY))
+server_sock = BluetoothSocket( RFCOMM )
+server_sock.bind(("", PORT_ANY))
 server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
 
 uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-advertise_service( server_sock, "SampleServer",
+advertise_service( server_sock, "TGA Bluetooth Server",
                    service_id = uuid,
                    service_classes = [ uuid, SERIAL_PORT_CLASS ],
                    profiles = [ SERIAL_PORT_PROFILE ])
 
 def bluetoothWorker(idx):
-    print("Waiting for connection on RFCOMM channel %d" % port)
+    print("Device {}: Waiting for connection on RFCOMM channel {}...".format(idx, port))
 
     client_sock, client_info = server_sock.accept()
-    print("Accepted connection from ", client_info)
+    print("Device {}: Accepted connection from {}.".format(idx, client_info))
 
-    try:
-        while True:
+    while True:
+        try:
             data = client_sock.recv(1024)
-            if len(data) == 0: break
-            print("received from {} {}".format(idx, data))
-            client_sock.send(data)
-    except IOError:
-        pass
+            if len(data) != 0:
+              client_sock.send(data+"2")
+        except IOError:
+            print("Device {}: Unable to connect...".format(idx))
+            print(IOError)
+            client_sock, client_info = server_sock.accept()
+            print("Device {}: Accepted connection from {}.".format(idx, client_info))
 
     client_sock.close()
     server_sock.close()
 
-
+print('Press `Ctrl+Shift+\` to Exit')
+print("Starting Threads...")
+    
 threads = []
 for i in range(6):
     t = threading.Thread(target=bluetoothWorker, args=(i,))
     threads.append(t)
     t.start()
 
-print("all done")
+print("Waiting for Connections...")
