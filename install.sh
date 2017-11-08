dcf3c5a8a349cb9b5c03677f5cc648fc8415dff8
Roshan Ravi <roshanravi10@gmail.com>
2017-11-07 21:10:58 -0500

    Circumvent User Input for Apt

--- install.sh
+++ install.sh
@@ -28,24 +28,24 @@ cd TGA-Server-2018
 echo "pi:TGA-4909" | sudo chpasswd
 
 # For Ethernet Tethering to Pi
-sudo apt-get install avahi-daemon
+sudo apt-get --yes --force-yes install avahi-daemon
 sudo update-rc.d avahi-daemon defaults
 
 # For Python Bluetooth Dependencies
-sudo apt-get install libboost-python-dev
-sudo apt-get install libboost-thread-dev
-sudo apt-get install libbluetooth-dev
-sudo apt-get install libglib2.0-dev
-sudo apt-get install python-bluez
+sudo apt-get --yes --force-yes install libboost-python-dev
+sudo apt-get --yes --force-yes install libboost-thread-dev
+sudo apt-get --yes --force-yes install libbluetooth-dev
+sudo apt-get --yes --force-yes install libglib2.0-dev
+sudo apt-get --yes --force-yes install python-bluez
 
 # For Bluetooth Pairing GUI (optional)
 # sudo apt-get install blueman
 
 # For Database Usage
-sudo apt-get install mongodb
+sudo apt-get --yes --force-yes install mongodb
 
 # For Node.JS Server Dependencies
-sudo apt-get install npm
+sudo apt-get --yes --force-yes install npm
 
 # Install Node.JS Dependencies
 npm install
