770303ece88e744a7c6a0e6c457f051c685463cd
Roshan Ravi <roshanravi10@gmail.com>
2017-11-08 17:06:02 -0500

    Revert "Remove Installer"
    This reverts commit 6b53d595255fa2862444d91d4bfc68f0312f4c56.

---
+++ install.sh
@@ -0,0 +1,51 @@
+#
+# The Green Alliance by Team 4909 <team4909@gmail.com>
+# - Author: Roshan Ravi <roshanravi10@gmail.com>
+#
+# This script is assuming the following is setup:
+# - Raspberry Pi 3 w/ at least 8GB SD card
+#       (16GB Recommended)
+# - The Pi is Connected to the Internet and can access 
+#       the apt and npm repositories.
+# - The Pi has SSH/VNC preconfigured
+#       This can be done via the GUI or on the terminal
+#       using the `sudo raspi-config` command.
+
+# This script can be pulled an installed on a Pi with the
+# following command:
+# sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/FRCteam4909/The-Green-Alliance/master/install.sh)"
+
+# Move to Desktop Directory
+cd ~/Desktop
+
+# Clone TGA Server Repository
+git clone https://github.com/FRCteam4909/TGA-Server-2018.git
+
+# Move to Newly Cloned Repository
+cd TGA-Server-2018
+
+# Change Password for Remote Access
+echo "pi:TGA-4909" | sudo chpasswd
+
+# For Ethernet Tethering to Pi
+sudo apt-get --yes --force-yes install avahi-daemon
+sudo update-rc.d avahi-daemon defaults
+
+# For Python Bluetooth Dependencies
+sudo apt-get --yes --force-yes install libboost-python-dev
+sudo apt-get --yes --force-yes install libboost-thread-dev
+sudo apt-get --yes --force-yes install libbluetooth-dev
+sudo apt-get --yes --force-yes install libglib2.0-dev
+sudo apt-get --yes --force-yes install python-bluez
+
+# For Bluetooth Pairing GUI (optional)
+# sudo apt-get install blueman
+
+# For Database Usage
+sudo apt-get --yes --force-yes install mongodb
+
+# For Node.JS Server Dependencies
+sudo apt-get --yes --force-yes install npm
+
+# Install Node.JS Dependencies
+npm install
