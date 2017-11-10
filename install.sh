082b68e7287898fac40b6eac00a62511a61c1334
Roshan Ravi <roshanravi10@gmail.com>
2017-11-10 16:25:05 -0500

    SPP

--- install.sh
+++ install.sh
@@ -29,6 +29,11 @@ sudo apt-get --yes install libbluetooth-dev
 sudo apt-get --yes install libglib2.0-dev
 sudo apt-get --yes install python-bluez
 
+sudo pip3 install pybluez
+sudo pip3 install requests
+
+sudo sed -i 's/bluetoothd/bluetoothd -C/g' /etc/systemd/system/dbus-org.bluez.service
+
 # For Bluetooth Pairing GUI (optional)
 # sudo apt-get install blueman
 
