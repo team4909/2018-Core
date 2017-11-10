4f146630ffa1aee1e62047f6af3a0f0207ace346
Roshan Ravi <roshanravi10@gmail.com>
2017-11-10 13:40:01 -0500

    Fix Typo

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@
 # This script can be pulled an installed on a Pi with the
 # following command:
 # sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/FRCteam4909/The-Green-Alliance/master/install.sh)"
-s
+
 # Clone TGA Server Repository
 git clone https://github.com/FRCteam4909/TGA-Server-2018.git
 
