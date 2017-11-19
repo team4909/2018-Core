d0f82d57746fc29fe046239bd3e1ad5dc9df6e90
Roshan Ravi <roshanravi10@gmail.com>
2017-11-19 02:16:34 -0500

    Maintain Connection State

--- client/android/bluetooth-plugin/www/BluetoothSPP.js
+++ client/android/bluetooth-plugin/www/BluetoothSPP.js
@@ -1,9 +1,29 @@
 var exec = require('cordova/exec');
 
-exports.initConnection = function(macAddr, success, error) {
-  exec(success, error, 'BluetoothSPP', 'initConnection', [macAddr]);
+exports.isConnected = false;
+
+exports.initConnection = function (macAddr, success, error) {
+    exec(function () {
+        exports.isConnected = true;
+
+        success();
+    }, function (err) {
+        exports.isConnected = false;
+
+        error(err);
+    }, 'BluetoothSPP', 'initConnection', [macAddr]);
 };
 
-exports.sendData = function(data, success, error) {
-  exec(success, error, 'BluetoothSPP', 'sendData', [data]);
+exports.sendData = function (data, success, error) {
+    if (exports.isConnected) {
+        exec(function () {
+            success();
+        }, function (err) {
+            exports.isConnected = false;
+
+            error(err);
+        }, 'BluetoothSPP', 'sendData', [data]);
+    } else {
+        error("The Server is not Connected.");
+    }
 };
