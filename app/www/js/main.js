fd58328771839b4c613fd81d763eb49f3045b1f3
Roshan Ravi <roshanravi10@gmail.com>
2017-12-27 23:16:27 -0500

    Templatify for SPA

--- app/www/js/main.js
+++ app/www/js/main.js
@@ -1,5 +1,11 @@
 $(function () {
     setTimeout(function () {
+        const templates = {
+            dashboard: Handlebars.compile($("#dashboard-template").html())
+        };
+
+        $(".main").html(templates.dashboard({}));
+
         $('.page-loader-wrapper').fadeOut();
     }, 50);
 });
