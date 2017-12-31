80aff066cc1c6ea2087bbff8152f42a707d0de06
Tom Orth <torth212@gmail.com>
2017-12-30 19:35:08 -0500

    Allow Hiding of Sidebar

---
+++ app/www/js/admin.js
@@ -0,0 +1,105 @@
+//Reference: https://github.com/gurayyarar/AdminBSBMaterialDesign/blob/gh-pages/js/admin.js
+$.AdminBSB = {};
+$.AdminBSB.options = {
+    leftSideBar: {
+        breakpointWidth: 1170        
+    }
+}
+$.AdminBSB.leftSideBar = {
+    activate: function () {
+        var _this = this;
+        var $body = $('body');
+        var $overlay = $('.overlay');
+
+        //Close sidebar
+        $(window).click(function (e) {
+            var $target = $(e.target);
+            if (e.target.nodeName.toLowerCase() === 'i') { $target = $(e.target).parent(); }
+
+            if (!$target.hasClass('bars') && _this.isOpen() && $target.parents('#leftsidebar').length === 0) {
+                if (!$target.hasClass('js-right-sidebar')) $overlay.fadeOut();
+                $body.removeClass('overlay-open');
+            }
+        });
+
+        $.each($('.menu-toggle.toggled'), function (i, val) {
+            $(val).next().slideToggle(0);
+        });
+
+        //When page load
+        $.each($('.menu .list li.active'), function (i, val) {
+            var $activeAnchors = $(val).find('a:eq(0)');
+
+            $activeAnchors.addClass('toggled');
+            $activeAnchors.next().show();
+        });
+
+        //Collapse or Expand Menu
+        $('.menu-toggle').on('click', function (e) {
+            var $this = $(this);
+            var $content = $this.next();
+
+            if ($($this.parents('ul')[0]).hasClass('list')) {
+                var $not = $(e.target).hasClass('menu-toggle') ? e.target : $(e.target).parents('.menu-toggle');
+
+                $.each($('.menu-toggle.toggled').not($not).next(), function (i, val) {
+                    if ($(val).is(':visible')) {
+                        $(val).prev().toggleClass('toggled');
+                        $(val).slideUp();
+                    }
+                });
+            }
+
+            $this.toggleClass('toggled');
+            $content.slideToggle(320);
+        });
+
+        //Set menu height
+        _this.setMenuHeight();
+        _this.checkStatuForResize(true);
+        $(window).resize(function () {
+            _this.setMenuHeight();
+            _this.checkStatuForResize(false);
+        });
+    },
+    setMenuHeight: function (isFirstTime) {
+        if (typeof $.fn.slimScroll != 'undefined') {
+            var configs = $.AdminBSB.options.leftSideBar;
+            var height = ($(window).height() - ($('.legal').outerHeight() + $('.user-info').outerHeight() + $('.navbar').innerHeight()));
+            var $el = $('.list');
+
+            //Scroll active menu item when page load, if option set = true
+            if ($.AdminBSB.options.leftSideBar.scrollActiveItemWhenPageLoad) {
+                var activeItemOffsetTop = $('.menu .list li.active')[0].offsetTop
+                if (activeItemOffsetTop > 150) $el.slimscroll({ scrollTo: activeItemOffsetTop + 'px' });
+            }
+        }
+    },
+    checkStatuForResize: function (firstTime) {
+        var $body = $('body');
+        var $openCloseBar = $('.navbar .navbar-header .bars');
+        var width = $body.width();
+
+        if (firstTime) {
+            $body.find('.content, .sidebar').addClass('no-animate').delay(1000).queue(function () {
+                $(this).removeClass('no-animate').dequeue();
+            });
+        }
+
+        if (width < $.AdminBSB.options.leftSideBar.breakpointWidth) {
+            $body.addClass('ls-closed');
+            $openCloseBar.fadeIn();
+        }
+        else {
+            $body.removeClass('ls-closed');
+            $openCloseBar.fadeOut();
+        }
+    },
+    isOpen: function () {
+        return $('body').hasClass('overlay-open');
+    }
+};
+
+$(function () {
+    $.AdminBSB.leftSideBar.activate()
+});
