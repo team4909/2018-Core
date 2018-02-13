01d13272a31c5908debb14fba2eb7f55603708da
Roshan Ravi <roshanravi10@gmail.com>
2018-02-12 20:28:11 -0500

    DB Conf + SSH

--- INSTALL.sh
+++ INSTALL.sh
@@ -9,14 +9,17 @@ service hostname start
 # For Ethernet Tethering to Pi
 sudo apt-get --yes install avahi-daemon
 sudo update-rc.d avahi-daemon defaults
+sudo systemctl enable ssh
+sudo systemctl start ssh
 
 # For Database Installation
 curl -sSL https://get.docker.com | sh
-docker run -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password -p 5984:5984 -d --restart=always couchdb
-curl -X PUT http://127.0.0.1:5984/_users
-curl -X PUT http://127.0.0.1:5984/_replicator
-curl -X PUT http://127.0.0.1:5984/_global_changes
-curl -X PUT http://127.0.0.1:5984/tga-2018
+docker run -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password -p 5984:5984 -d --restart=always matthiasg/rpi-couchdb
+sleep 1
+curl -X PUT http://admin:password@127.0.0.1:5984/_users
+curl -X PUT http://admin:password@127.0.0.1:5984/_replicator
+curl -X PUT http://admin:password@127.0.0.1:5984/_global_changes
+curl -X PUT http://admin:password@127.0.0.1:5984/tga-2018
 
 # For Webhook Server (Node)
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
