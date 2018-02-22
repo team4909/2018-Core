1e607dcbbd85efc41c068e55bc65ee4c424904eb
Roshan Ravi <roshanravi10@gmail.com>
2018-02-22 14:36:59 -0500

    Update Cache & Root Privs

--- INSTALL.sh
+++ INSTALL.sh
@@ -13,7 +13,7 @@ sudo systemctl start ssh
 
 # For Database Installation
 curl -sSL https://get.docker.com | sh
-docker run -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password -p 5984:5984 -d --restart=always matthiasg/rpi-couchdb
+sudo docker run -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password -p 5984:5984 -d --restart=always matthiasg/rpi-couchdb
 sleep 1
 
 HOST=http://admin:password@127.0.0.1:5984
