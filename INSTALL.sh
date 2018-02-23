ea41152ab00b0fc06a9a81fc3e5af5c317c0400d
Roshan Ravi <roshanravi10@gmail.com>
2018-02-22 20:01:15 -0500

    Verbose Script

--- INSTALL.sh
+++ INSTALL.sh
@@ -9,23 +9,44 @@ cd /home/pi/the-green-alliance
 git checkout -f master
 git pull
 
-# Change Hostname
+echo
+echo
+echo "The Green Alliance Pi Installer Script will do the following: "
+echo " - change device hostname to the 'the-green-alliance' "
+echo " - enable ethernet tethering"
+echo " - enable SSH for remote access"
+echo " - install and configure the CouchDB datastore"
+echo " - install and configure the TGA Bluetooth syncing server"
+echo
+echo
+echo "You may press Ctrl+C now to abort this script."
+echo "+ sleep 20"
+sleep 20
+
+# Change Hostname to 'the-green-alliance'
+echo "Changing Hostname to 'the-green-alliance' ..."
 echo "the-green-alliance" | sudo tee /etc/hostname > /dev/null
 sudo sed -i 's/127.0.1.1.*/127.0.1.1\t'"the-green-alliance"'/g' /etc/hosts
 sudo hostname "the-green-alliance"
 
 # For Ethernet Tethering to Pi
-sudo apt-get --yes install avahi-daemon
+echo "Enabling Ethernet Tethering..."
+sudo apt-get -qq --yes install avahi-daemon
 sudo update-rc.d avahi-daemon defaults
+
+# Enable SSH
+echo "Enabling SSH Server..."
 sudo systemctl enable ssh
 sudo systemctl start ssh
 
 # For Database Installation
+echo "Installing CouchDB Server..."
 curl -sSL https://get.docker.com | sh
 sudo docker run -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password -p 5984:5984 -d --restart=always matthiasg/rpi-couchdb
 sleep 1
 
 # Config DB
+echo "Configuring CouchDB Server..."
 HOST=http://admin:password@127.0.0.1:5984
 curl -X PUT $HOST/_users
 curl -X PUT $HOST/_replicator
@@ -37,33 +58,33 @@ curl -X PUT $HOST/_node/nonode@nohost/_config/cors/credentials -d '"true"'
 curl -X PUT $HOST/_node/nonode@nohost/_config/cors/methods -d '"GET, PUT, POST, HEAD, DELETE"'
 curl -X PUT $HOST/_node/nonode@nohost/_config/cors/headers -d '"accept, authorization, content-type, origin, referer, x-csrf-token"'
 
-# For Webhook Server (Node)
+# For Webhook Server Dependencies
+echo "Configuring TGA Bluetooth Worker Daemon..."
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-# For Webhook Server (Node Dependencies)
+sudo apt-get -qq --yes install nodejs
 cd /home/pi/the-green-alliance/bluetooth-worker
 sudo npm install
 
-# For Python Bluetooth Dependencies
-sudo apt-get --yes install libboost-python-dev
-sudo apt-get --yes install libboost-thread-dev
-sudo apt-get --yes install libbluetooth-dev
-sudo apt-get --yes install libglib2.0-dev
-sudo apt-get --yes install bluez
-sudo apt-get --yes install python-bluez
-
-# For Python Bluetooth Libraries
+# For Python Bluetooth Dependencies/Libraries
+echo "Installing Bluetooth Dependencies..."
+sudo apt-get -qq --yes install libboost-python-dev
+sudo apt-get -qq --yes install libboost-thread-dev
+sudo apt-get -qq --yes install libbluetooth-dev
+sudo apt-get -qq --yes install libglib2.0-dev
+sudo apt-get -qq --yes install bluez
+sudo apt-get -qq --yes install python-bluez
 sudo pip3 install pybluez
 sudo pip3 install requests
 
 # Start Bluetooth Daemon
+echo "Starting Bluetooth Daemon..."
 sudo sed -i 's/bluetoothd/bluetoothd -C/g' /lib/systemd/system/bluetooth.service
 sudo systemctl daemon-reload
 sudo service bluetooth restart
 sudo sdptool add SP
 
 # Start Bluetooth Worker Daemon
+echo "Starting TGA Bluetooth Worker Daemon..."
 sudo cp /home/pi/the-green-alliance/bluetooth-worker/bt-worker.service /lib/systemd/system/
 sudo chmod 644 /lib/systemd/system/bt-worker.service
 chmod +x /home/pi/the-green-alliance/bluetooth-worker/server.js
@@ -71,6 +92,7 @@ sudo systemctl daemon-reload
 sudo systemctl enable bt-worker.service
 sudo systemctl start bt-worker.service
 
-sleep 5
-echo "TGA DB Daemon Status: "
-sudo systemctl status bt-worker.service
+# Inform User to Reboot
+echo
+echo "Please reboot to complete the installation."
+echo
