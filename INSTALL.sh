c5eb8dc4aa232d7e65ca8db6b82877601d1f50b2
Roshan Ravi <roshanravi10@gmail.com>
2018-02-26 18:49:54 -0500

    Update script

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-# Clone Repo
-cd /home/pi
-git clone https://github.com/FRCteam4909/the-green-alliance.git
-
-# Update Repo
-cd /home/pi/the-green-alliance
-git checkout -f master
-git pull
-
 # TL;DR of Script
 echo
-echo "The Green Alliance Pi Installer Script will do the following: "
+echo "THE GREEN ALLIANCE SCOUTING PLATFORM"
+echo " - INSTALLER SCRIPT"
+echo
+echo" will do the following: "
 echo " - change device hostname to the 'the-green-alliance' "
 echo " - enable ethernet tethering"
 echo " - enable SSH for remote access"
@@ -22,30 +16,40 @@ echo "You may press Ctrl+C now to abort this script."
 echo "+ sleep 20"
 sleep 20
 
+# Clone Repo
+cd /home/pi
+git clone https://github.com/FRCteam4909/the-green-alliance.git
+
+# Update Repo
+cd /home/pi/the-green-alliance
+git checkout -f master
+git pull
+
 # Change Hostname to 'the-green-alliance'
-echo "Changing Hostname to 'the-green-alliance' ..."
+echo "[TGA] Changing Hostname to 'the-green-alliance' ..."
 echo "the-green-alliance" | sudo tee /etc/hostname > /dev/null
 sudo sed -i 's/127.0.1.1.*/127.0.1.1\t'"the-green-alliance"'/g' /etc/hosts
 sudo hostname "the-green-alliance"
 
 # For Ethernet Tethering to Pi
-echo "Enabling Ethernet Tethering..."
+echo "[TGA] Enabling Ethernet Tethering..."
 sudo apt-get -qq --yes install avahi-daemon
 sudo update-rc.d avahi-daemon defaults
 
 # Enable SSH
-echo "Enabling SSH Server..."
+echo "[TGA] Enabling SSH Server..."
 sudo systemctl enable ssh
 sudo systemctl start ssh
 
 # For Database Installation
-echo "Installing CouchDB Server..."
+echo "[TGA] Installing CouchDB Server..."
 curl -sSL https://get.docker.com | sh
 sudo docker run -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=password -p 5984:5984 -d --restart=always --name tga-couchdb matthiasg/rpi-couchdb
-sleep 1
 
+echo "+ sleep 5"
+sleep 5
 # Config DB
-echo "Configuring CouchDB Server..."
+echo "[TGA] Configuring CouchDB Server..."
 HOST=http://admin:password@127.0.0.1:5984
 curl -X PUT $HOST/_users
 curl -X PUT $HOST/_replicator
@@ -58,14 +62,14 @@ curl -X PUT $HOST/_node/nonode@nohost/_config/cors/methods -d '"GET, PUT, POST, 
 curl -X PUT $HOST/_node/nonode@nohost/_config/cors/headers -d '"accept, authorization, content-type, origin, referer, x-csrf-token"'
 
 # For Webhook Server Dependencies
-echo "Configuring TGA Bluetooth Worker Daemon..."
+echo "[TGA] Configuring Bluetooth Worker Daemon..."
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get -qq --yes install nodejs
 cd /home/pi/the-green-alliance/bluetooth-worker
 sudo npm install
 
 # For Python Bluetooth Dependencies/Libraries
-echo "Installing Bluetooth Dependencies..."
+echo "[TGA] Installing Bluetooth Dependencies..."
 sudo apt-get -qq --yes install libboost-python-dev
 sudo apt-get -qq --yes install libboost-thread-dev
 sudo apt-get -qq --yes install libbluetooth-dev
@@ -76,14 +80,14 @@ sudo pip3 install pybluez
 sudo pip3 install requests
 
 # Start Bluetooth Daemon
-echo "Starting Bluetooth Daemon..."
+echo "[TGA] Starting OS Bluetooth Daemon..."
 sudo sed -i 's/bluetoothd/bluetoothd -C/g' /lib/systemd/system/bluetooth.service
 sudo systemctl daemon-reload
 sudo systemctl restart bluetooth
 sudo sdptool add SP
 
 # Start Bluetooth Worker Daemon
-echo "Starting TGA Bluetooth Worker Daemon..."
+echo "[TGA] Starting TGA Bluetooth Worker Daemon..."
 sudo cp /home/pi/the-green-alliance/bluetooth-worker/bt-worker-hook.service /lib/systemd/system/
 sudo cp /home/pi/the-green-alliance/bluetooth-worker/bt-worker.service /lib/systemd/system/
 sudo chmod 644 /lib/systemd/system/bt-worker-hook.service
@@ -97,7 +101,33 @@ sudo systemctl enable bt-worker.service
 sudo systemctl restart bt-worker-hook.service
 sudo systemctl restart bt-worker.service
 
-# Inform User to Reboot
 echo
+echo "THE GREEN ALLIANCE SCOUTING PLATFORM"
+echo "    BY TEAM 4909 (THE BILLERICA BIONICS)"
+echo
+echo "DEBUGGING NOTES"
+echo "For debugging purposes, the Pi is now configured"
+echo "to allow SSH access at 'the-green-alliance.local' when"
+echo "when tethered via ethernet cable."
+echo 
+echo "In addition, the local CouchDB server can be "
+echo "accessed at 'the-green-alliance.local:5984'"
+echo "using the credentials below:"
+echo
+echo "    Username: admin"
+echo "    Password: password"
+echo
+echo "BLUETOOTH CONFIGURATION"
+echo "Take note of the following Bluetooth MAC Address. This"
+echo "identifier is needed to configure the Bluetooth portion"
+echo "of the transfer mechanism. Ex. 00:25:96:12:34:56"
+echo
+hcitool dev
+echo
+echo "The GitHub documentation has detailed information"
+echo "on how to setup Bluetooth on Android devices for"
+echo "TGA. If you need additional help, please use the"
+echo "#issues channel on the TGA Slack."
+echo 
 echo "Please reboot to complete the installation."
 echo
