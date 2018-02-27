3d5036e33510d97679064935b8946ecab1173acf
Roshan Ravi <roshanravi10@gmail.com>
2018-02-26 20:35:37 -0500

    Verbose Script

--- INSTALL.sh
+++ INSTALL.sh
@@ -52,16 +52,15 @@ echo "+ sleep 5"
 sleep 5
 # Config DB
 echo "[TGA] Configuring CouchDB Server..."
-HOST=http://admin:password@127.0.0.1:5984
-curl -X PUT $HOST/_users
-curl -X PUT $HOST/_replicator
-curl -X PUT $HOST/_global_changes
-curl -X PUT $HOST/tga-2018
-curl -X PUT $HOST/_node/nonode@nohost/_config/httpd/enable_cors -d '"true"'
-curl -X PUT $HOST/_node/nonode@nohost/_config/cors/origins -d '"*"'
-curl -X PUT $HOST/_node/nonode@nohost/_config/cors/credentials -d '"true"'
-curl -X PUT $HOST/_node/nonode@nohost/_config/cors/methods -d '"GET, PUT, POST, HEAD, DELETE"'
-curl -X PUT $HOST/_node/nonode@nohost/_config/cors/headers -d '"accept, authorization, content-type, origin, referer, x-csrf-token"'
+curl -X PUT http://admin:password@127.0.0.1:5984/_users
+curl -X PUT http://admin:password@127.0.0.1:5984/_replicator
+curl -X PUT http://admin:password@127.0.0.1:5984/_global_changes
+curl -X PUT http://admin:password@127.0.0.1:5984/tga-2018
+curl -X PUT http://admin:password@127.0.0.1:5984/_node/nonode@nohost/_config/httpd/enable_cors -d '"true"'
+curl -X PUT http://admin:password@127.0.0.1:5984/_node/nonode@nohost/_config/cors/origins -d '"*"'
+curl -X PUT http://admin:password@127.0.0.1:5984/_node/nonode@nohost/_config/cors/credentials -d '"true"'
+curl -X PUT http://admin:password@127.0.0.1:5984/_node/nonode@nohost/_config/cors/methods -d '"GET, PUT, POST, HEAD, DELETE"'
+curl -X PUT http://admin:password@127.0.0.1:5984/_node/nonode@nohost/_config/cors/headers -d '"accept, authorization, content-type, origin, referer, x-csrf-token"'
 
 # For Webhook Server Dependencies
 echo "[TGA] Configuring Bluetooth Worker Daemon..."
