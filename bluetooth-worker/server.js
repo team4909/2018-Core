fc5d1450aac78b3dda1ed5c3ae5567854891f8c6
Roshan Ravi <roshanravi10@gmail.com>
2018-02-27 20:57:50 -0500

    Fix

--- bluetooth-worker/server.js
+++ bluetooth-worker/server.js
@@ -12,10 +12,8 @@ app.use(bodyParser.json()).use(function (error, req, res, next) {
 
 // Handle JSON POST Req, Sent By Py. as Data Flows In
 app.post('/new_msg', function (req, res) {
-    const msg_data = req.body;
-
     console.log("New Data:");
-    console.dir(msg_data);
+    console.dir(req.body);
 
     db.insert(msg_data, msg_data._id, function (err, body, header) {
         if (err) {
